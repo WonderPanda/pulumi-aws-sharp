134f2fefc355d5d638b3c5f9fdfabb180b9cb459
Jesse Carter <jesse.r.carter@gmail.com>
2018-10-20 10:39:02 -0400

    Started making demo app

--- index.ts
+++ index.ts
@@ -3,32 +3,35 @@ import axios from 'axios';
 import express from 'express';
 import sharp from 'sharp';
 
-const server = new cloud.HttpServer('sharp-test', () => {
+export const makeApp = () => {
   const app = express();
-
+  
   app.get('*', async (req, res) => {
     const response = await axios.get(
-      'https://www.nachosandchill.com/static/09a6162cdca653e8ad7f026b8dbaf21b-9a729b3adaa6efa713eb597b7426e641-384f5.webp',
+      'https://en.wikipedia.org/wiki/Tiger#/media/File:Royal_Bengal_Tiger_at_Kanha_National_Park.jpg',
       {
         responseType: 'arraybuffer'
       }
     );
-
+  
     const imageBuffer = Buffer.from(response.data, 'binary');
-
-    // await sharp(imageBuffer)
+    
     const newBuffer = await sharp(imageBuffer)
       .resize(800, 500)
       .toBuffer();
-
+  
     res.writeHead(200, {
       'Content-disposition': 'attachment;filename=' + 'test.webp',
       'Content-Length': newBuffer.length
     });
     res.end(newBuffer);
   });
-
+  
   return app;
+}
+
+const server = new cloud.HttpServer('sharp-test', () => {
+  return makeApp();
 });
 
 exports.endpoint = server.url;
